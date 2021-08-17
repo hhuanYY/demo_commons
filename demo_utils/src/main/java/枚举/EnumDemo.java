c59548a134e821e6c79d9fd240bf6805f6059422
yuanyinhuan <yuanyinhuan@dossen.com>
2021-08-17 11:58:42 +0800

    枚举demo

---
+++ demo_utils/src/main/java/枚举/EnumDemo.java
@@ -0,0 +1,9 @@
+package 枚举;
+
+/**
+ * @author: yuanyinhuan
+ * @date: 2021/8/17 11:22
+ */
+public enum EnumDemo {
+    ONE,TWO,THREE,FOUR,FIVE,SIX
+}
---
+++ demo_utils/src/main/java/枚举/MainDemo.java
@@ -0,0 +1,24 @@
+package 枚举;
+
+/**
+ * @author: yuanyinhuan
+ * @date: 2021/8/17 11:23
+ */
+public class MainDemo {
+    public static void main(String[] args) {
+
+        /** 枚举常量转为字符串, 可用于比较
+         * int toString()
+         * int name()
+         */
+        String name1 = EnumDemo.ONE.toString();
+        String name2 = EnumDemo.ONE.name();
+
+
+        /** 返回此枚举常量的序数,初始值从0开始
+         * int ordinal()
+         */
+        int ordinal = EnumDemo.ONE.ordinal();
+
+    }
+}
