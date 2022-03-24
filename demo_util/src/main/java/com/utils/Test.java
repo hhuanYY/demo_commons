ca5c0afa6e87238bdd744d5295e35ba5bac272a3
yuanyinhuan <yuanyinhuan@dossen.com>
2022-03-24 15:10:39 +0800

    测试文件

--- demo_util/src/main/java/com/utils/Test.java
+++ demo_util/src/main/java/com/utils/Test.java
@@ -1,5 +1,8 @@
 package com.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: yuanyinhuan
  * @date: 2022/3/3 17:31
@@ -7,17 +10,53 @@ package com.utils;
 public class Test {
     public static void main(String[] args) throws Exception {
 
-        try {
-            String str = "test";
-            System.err.println("异常开始");
-            str.substring(0, 100);
-            System.err.println("异常结束");
-        } catch (Exception e) {
-            System.err.println("处理异常");
-            throw new Exception("下标越界");
+
+//        String str = "1.45";
+//        double v1 = Double.parseDouble(str);
+//        double v2 = 0.35;
+//        if (v1 > v2) {
+//            System.err.println("0.45 > 0.35");
+//        }
+//
+        List<String> hotelList = new ArrayList<>();
+        hotelList.add("0020001");
+        hotelList.add("0020002");
+        hotelList.add("0020003");
+
+        List<String> indexCodeList = new ArrayList<>();
+        indexCodeList.add("index_occ");
+        indexCodeList.add("index_adr");
+
+        List<TestDemo> successStore = new ArrayList<>();
+
+        for (String hotel : hotelList) {
+            for (String indexCode : indexCodeList) {
+                //1. 查询某个酒店对应的指标值
+                TestValue value = methods01(hotel, indexCode);
+                if ("index_occ".equals(indexCode)) {
+                    if (Double.parseDouble(value.getValue()) > new Double(0.50)) {
+                        TestDemo testDemo = new TestDemo(hotel, indexCode);
+                        successStore.add(testDemo);
+                    }
+
+                } else if ("index_adr".equals(indexCode)) {
+                    if (Double.parseDouble(value.getValue()) > new Double(50)) {
+                        TestDemo testDemo = new TestDemo(hotel, indexCode);
+                        successStore.add(testDemo);
+                    }
+
+                }
+            }
         }
 
-        System.err.println("跳出方法");
+        System.err.println(successStore);
+
 
     }
+
+    private static TestValue methods01(String hotel, String indexCode) {
+        return new TestValue("0020001", "0.58");
+    }
+
+
 }
